6d91d04a976822a786db790c7585b384073e2fc8
Bilal Kocak <kocakbilal04@gmail.com>
2020-09-04 23:13:51 +0300

    Fixed component style

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 const Search = () => {
+    const [searchText, setSearchText] = useState('')
+
+    function onChangeSearchInput(value) {
+        setSearchText(value)
+    };
+
     return (
         <div className={'mainPageSearchSection'}>
+            <div className={'searchPageTitle'}>
+                <h1>
+                    OMDb
+                </h1>
+                <h3>
+                    The Open Movie Database
+                </h3>
+            </div>
             <div className={'searchArea'}>
-                <input placeholder="input search text" onSearch={value => console.log(value)} enterButton/>
+                <input placeholder="input search text"
+                       onChange={(e) => onChangeSearchInput(e.target.value)}
+                       value={searchText}/>
+                <button>Ara</button>
             </div>
         </div>
     );
