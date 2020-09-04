921b77b2e447306d70cb287c21fd2c1ce0317189
Bilal Kocak <kocakbilal04@gmail.com>
2020-09-04 23:56:50 +0300

    Added triangle to bottom of search area

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -10,24 +10,27 @@ const Search = () => {
 
     return (
         <div className={'mainPageSearchSection'}>
-            <div className={'searchPageTitle'}>
-                <h1>
-                    OMDb
-                </h1>
-                <h3>
-                    The Open Movie Database
-                </h3>
-                <div>
-                    <a href="#top10">Top10</a>
-                    <a href="#editors">Editor's choices</a>
+            <div className="content">
+                <div className={'searchPageTitle'}>
+                    <h1>
+                        OMDb
+                    </h1>
+                    <h3>
+                        The Open Movie Database
+                    </h3>
+                    <div>
+                        <a href="#top10">Top10</a>
+                        <a href="#editors">Editor's choices</a>
+                    </div>
+                </div>
+                <div className={'searchArea'}>
+                    <input placeholder="Search movie..."
+                           onChange={(e) => onChangeSearchInput(e.target.value)}
+                           value={searchText}/>
+                    <button>Search</button>
                 </div>
             </div>
-            <div className={'searchArea'}>
-                <input placeholder="Search movie..."
-                       onChange={(e) => onChangeSearchInput(e.target.value)}
-                       value={searchText}/>
-                <button>Search</button>
-            </div>
+            <div className={'triangle'}/>
         </div>
     );
 };
