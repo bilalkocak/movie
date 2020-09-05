301f67a28c98a04eabb1b6640306190460a7bcfe
Bilal Kocak <kocakbilal04@gmail.com>
2020-09-05 16:59:12 +0300

    Created suggestion component for listing movie with style

---
+++ components/TopTen/Suggestion.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import MovieCard from "../MovieCard/MovieCard";
+
+const Suggestion = ({id, title, films}) => {
+
+    return (
+        <div className={'suggestion'} id={id}>
+            <h4 className={'title'}>
+                #{title}
+            </h4>
+            <div className={'films'}>
+                {films.map((film, index) => {
+                    return (<MovieCard film={film} style={{height: 150}} key={index}>{film}</MovieCard>)
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Suggestion;
