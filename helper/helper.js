d1d44a2fb5d89167f10291d8f40ed342b4eb0775
Bilal Kocak <kocakbilal04@gmail.com>
2020-09-06 01:01:36 +0300

    Moved watchlist functions to redux action

--- helper/helper.js
+++ helper/helper.js
@@ -1,17 +1,7 @@
-export function addFilmToWatchList(id) {
-    let watchList = localStorage.getItem("watchList") ? JSON.parse(localStorage.getItem("watchList")) : [];
-    !watchList.includes(id) && watchList.push(id);
-    localStorage.setItem('watchList', JSON.stringify(watchList))
-}
-
-export function deleteFilmFromWatchList(id) {
-    let watchList = localStorage.getItem("watchList") ? JSON.parse(localStorage.getItem("watchList")) : [];
-    watchList.includes(id) && watchList.splice(watchList.findIndex(movieId => movieId === id), 1);
-    localStorage.setItem('watchList', JSON.stringify(watchList))
-}
-
 export function isOnWatchList(id) {
-    // let watchList = localStorage.getItem("watchList") ? JSON.parse(localStorage.getItem("watchList")) : [];
-    // return watchList.includes(id)
+    if (localStorage.getItem("watchList")) {
+        let watchList = localStorage.getItem("watchList") ? JSON.parse(localStorage.getItem("watchList")) : [];
+        return watchList.includes(id)
+    }
     return false
 }
