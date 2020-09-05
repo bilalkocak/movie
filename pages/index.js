45219011d179f0b1a5c02e843e100d6710613ecb
Bilal Kocak <kocakbilal04@gmail.com>
2020-09-05 16:59:58 +0300

    Implemented suggestion component

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,34 @@
 import {useDispatch} from "react-redux";
-import {useEffect} from "react"
+import React, {useEffect, useState} from "react"
 import {fetchPosts} from "../store/actions/postAction";
 import Layout from "../components/Layout";
 import Search from "../components/Search/Search";
+import Suggestion from "../components/TopTen/Suggestion";
+import axios from "axios";
 
 
 export default function Home() {
     const dispatch = useDispatch()
+    const [topTen, setTopTen] = useState([])
+    const [editorChoices, setEditorChoices] = useState([])
+
+    function getTopFilms() {
+        axios.get('/api/editorsChoices').then(res => setEditorChoices(res.data.films)).catch(error => console.log(error))
+    }
+
+    function getEditorsChoices() {
+        axios.get('/api/topFilms').then(res => setTopTen(res.data.films)).catch(error => console.log(error))
+    }
+
     useEffect(() => {
-        dispatch(fetchPosts())
+        getTopFilms()
+        getEditorsChoices()
     }, [])
     return (
         <Layout>
             <Search/>
+            <Suggestion id={'top10'} title={'Top 10'} films={topTen}/>
+            <Suggestion id={'editors'} title={'Editor\'s Choices'} films={editorChoices}/>
         </Layout>
     )
 }
