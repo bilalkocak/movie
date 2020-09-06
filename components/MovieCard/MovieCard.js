cd5b031810259209bf9bb1b88a4e7488e207f4e7
Bilal Kocak <kocakbilal04@gmail.com>
2020-09-06 21:46:57 +0300

    Refactor for search result

--- components/MovieCard/MovieCard.js
+++ components/MovieCard/MovieCard.js
@@ -8,15 +8,20 @@ import Link from "next/link";
 import Button from "../Common/Button/Button";
 
 
-const MovieCard = ({film}) => {
+const MovieCard = ({filmId, filmObject}) => {
     const [filmData, setFilmData] = useState({})
     const [loading, setLoading] = useState(true)
     useEffect(() => {
-        axios.get(`${API_URL}i=${film}`).then(res => {
-            setFilmData(res.data)
+        if (!filmId) {
+            setFilmData(filmObject)
             setLoading(false)
-        })
-            .catch(error => error.log)
+        } else {
+            axios.get(`${API_URL}i=${filmId}`).then(res => {
+                setFilmData(res.data)
+                setLoading(false)
+            })
+                .catch(error => error.log)
+        }
     }, [])
     return (
         <>
@@ -36,20 +41,24 @@ const MovieCard = ({film}) => {
                                 {filmData.Title}
                             </div>
                             <div className="movieOtherInfo">
-                                {filmData.Year} &middot; {filmData.Genre} &middot; {filmData.Runtime}
+                                {filmData.Year} &middot; {filmId ? filmData.Genre : filmData.Type} {filmId && <>&middot; {filmData.Runtime}</>}
                             </div>
-                            <div className="movieRating">
+                            {filmId &&
+                            <>
+                                <div className="movieRating">
                                         <span className="movieRate">
                                             <CountUp start={0.00} end={parseFloat(filmData.imdbRating)} decimal=","
                                                      decimals={1}
                                                      duration={2}/>
                                            </span>/10 <strong style={{fontSize: 12}}>IMDB</strong>
-                            </div>
-                            <RatingBar rate={filmData.imdbRating} width={'100%'}/>
+                                </div>
+                                <RatingBar rate={filmData.imdbRating} width={'100%'}/>
+                            </>
+                            }
                         </div>
 
                         <Link href={`detail?id=${filmData.imdbID}`}>
-                            <Button text={'MOVIE DETAILS'} className={'button'}/>
+                            <Button text={'MOVIE DETAILS'}/>
                         </Link>
 
                     </div>
